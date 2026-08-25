a8119af10fa4afd8931bec6e44c25951519446fc
Akarshan <poudelakarshan123@gmail.com>
2026-08-25 15:39:28 +0545

    Register URL Endpoint

--- apps/academics/urls.py
+++ apps/academics/urls.py
@@ -5,4 +5,5 @@ app_name = 'academics'
 
 urlpatterns = [
     path('marks/entry/<int:exam_id>/<int:subject_id>/', views.mark_entry_view, name='mark_entry'),
+    path('export/iemis/', views.export_iemis_excel, name='export_iemis'), # <-- ADD THIS ROUTE
 ]
