f2145028fd7ccabcbd3f28a42796423eaabca50a
Akarshan <poudelakarshan123@gmail.com>
2026-09-01 15:21:46 +0545

    feat(users): enhance user admin interface with additional fields and filters

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,24 +5,44 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     # Columns displayed in the user list table
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'classroom', 'phone_number', 'is_staff')
     
     # Sidebar filters
-    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('role', 'classroom', 'is_staff', 'is_superuser', 'is_active')
     
     # Search functionality
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'citizenship_no', 'phone_number')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'citizenship_no', 'phone_number', 'father_name', 'mother_name')
     
-    # Group custom government fields in the user edit view
+    # Group custom government & student fields in the user edit view
     fieldsets = UserAdmin.fieldsets + (
-        ('Government SMS Attributes', {
-            'fields': ('role', 'phone_number', 'citizenship_no')
+        ('Government SMS & Student Details', {
+            'fields': (
+                'role', 
+                'classroom', 
+                'phone_number', 
+                'citizenship_no', 
+                'date_of_birth', 
+                'father_name', 
+                'mother_name', 
+                'parent_phone', 
+                'address'
+            )
         }),
     )
     
-    # Group custom government fields in the user creation view
+    # Group custom government & student fields in the user creation view
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Government SMS Attributes', {
-            'fields': ('role', 'phone_number', 'citizenship_no')
+        ('Government SMS & Student Details', {
+            'fields': (
+                'role', 
+                'classroom', 
+                'phone_number', 
+                'citizenship_no', 
+                'date_of_birth', 
+                'father_name', 
+                'mother_name', 
+                'parent_phone', 
+                'address'
+            )
         }),
     )
